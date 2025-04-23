f4fcb99ff3807eba3f0ebe0ee1e1f5d0126123fb
Devvil-debug34 <kymap_2014@mail.ru>
2025-04-23 15:24:34 +0300

    Inicial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/init/InitUserToDB.java
@@ -0,0 +1,50 @@
+package ru.kata.spring.boot_security.demo.init;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import javax.annotation.PostConstruct;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class InitUserToDB {
+    private final UserService userService;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public InitUserToDB(UserService userService, RoleService roleService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (roleService.findAll().isEmpty()) {
+
+
+            Role adminRole = new Role("ROLE_ADMIN");
+            Role userRole = new Role("ROLE_USER");
+
+            // set roles
+            Set<Role> rolesAdmin = new HashSet<>();
+            Set<Role> rolesUser = new HashSet<>();
+            rolesAdmin.add(adminRole);
+            rolesUser.add(userRole);
+            User admin = new User("Admin", "Admin", "admin@admin.ru", "admin", rolesAdmin);
+            User user = new User("User", "User", "user@user.ru", "user", rolesUser);
+            roleService.add(adminRole);
+            roleService.add(userRole);
+            userService.add(admin);
+            userService.add(user);
+
+        }
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImp.java
@@ -0,0 +1,37 @@
+package ru.kata.spring.boot_security.demo.services;
+
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.RoleRepository;
+import ru.kata.spring.boot_security.demo.dao.UserRepository;
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImp implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImp(RoleRepository roleRepository, UserRepository userRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void add(Role role) {
+        roleRepository.save(role);
+    }
+
+    @Override
+    public Role findByName(String name) {
+        return (Role) roleRepository.findByName(name);
+    }
+}
